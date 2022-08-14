1efda1406a3425f462fd9ab6b244f166580338d1
Dnyanesh Mali <malidnyanesh1911@gmail.com>
2022-08-14 19:53:09 +0530

    form validation

---
+++ main.js
@@ -0,0 +1,30 @@
+let id = (id) => document.getElementById(id);
+let classes = (classes) => document.getElementsByClassName(classes);
+
+let username = id("username");
+let email = id("email");
+let password = id("password");
+let form = id("form");
+
+let errorMsg = classes("error");
+let successIcon = classes("success-icon");
+let failureIcon = classes("failure-icon");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  engine(username, 0, "Username cannot be blank");
+  engine(email, 1, "Email cannot be blank");
+  engine(password, 2, "Password cannot be blank");
+});
+
+let engine = (id, serial, message) => {
+  if (id.value.trim() === "") {
+    errorMsg[serial].innerHTML = message;
+    failureIcon[serial].style.opacity = "1";
+    successIcon[serial].style.opacity = "0";
+  } else {
+    errorMsg[serial].innerHTML = "";
+    failureIcon[serial].style.opacity = "0";
+    successIcon[serial].style.opacity = "1";
+  }
+};
